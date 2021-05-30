ea5efea967f49bd480f5c1ec0c63dbdb10bb13d1
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-30 18:43:21 +0100

    created user model

---
+++ routes/api/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    defaulat: Date.now
+  }
+})
+
+module.exports = User = mongoose.model('user', UserSchema);
