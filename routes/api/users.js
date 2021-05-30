76152c8005078718ed77963f6fb3db3f2e4c0493
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-30 21:48:59 +0100

    saving user in database and validating

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    register user
@@ -17,12 +21,53 @@ router.post(
 		).isLength({ min: 6 })
 	],
 
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.send('User route');
+
+		const { name, email, password } = req.body;
+
+		try {
+			// see if user exists
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+			}
+
+			// Get users gravatar
+			const avatar = gravatar.url(email, {
+				s: '200',
+				r: 'pg',
+				d: 'mm'
+			});
+
+      // create new user
+			user = new User({
+				name,
+				email,
+				avatar,
+				password
+			});
+
+			// Encrypt password
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+      console.log(`${user.password} password`)
+
+			await user.save();
+
+			// Return jsonwebtokens
+
+			res.send('User registered');
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
 	}
 );
 
